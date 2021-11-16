ad4ac398f7245ed366557fb2ef10d318c162bd6c
paulovarao <paulovarao@gmail.com>
2021-11-16 15:33:12 -0300

    First commit: project created

---
+++ src/main/java/com/varaodev/spatialserver/SpatialServerApplication.java
@@ -0,0 +1,13 @@
+package com.varaodev.spatialserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpatialServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpatialServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/varaodev/spatialserver/SpatialServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.varaodev.spatialserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpatialServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
